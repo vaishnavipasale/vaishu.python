e81db2a606b1237ab1c2d2d47a48668632be339d
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-17 21:23:37 +0530

    Create greetuser.py

---
+++ greetuser.py
@@ -0,0 +1,3 @@
+#Create a Python program that takes the user's name as input and then uses that input to greet the user personally.#
+name = input("Enter your name")
+print(f"hello {name},have nice day")
