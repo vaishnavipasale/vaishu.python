def6b3923646887968468089d538feb71057669b
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-19 16:57:25 +0530

    Create minmax.py

---
+++ minmax.py
@@ -0,0 +1,4 @@
+Find the largest and smallest elements in a list?
+list1 = [100, 20, 330 , 4 , 45 , 0]
+print(f" maximum number of list is :" ,max(list1))
+print(f"minimum of list is :" ,min(list1))
