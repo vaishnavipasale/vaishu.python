369df7ad8d7489b8750aa72b0164a9ba5da898f8
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-19 21:48:39 +0530

    Create matrixtrapose.py

---
+++ matrixtrapose.py
@@ -0,0 +1,8 @@
+Create a matrix and find its transpose.
+A = [1 , 2 , 3],[4 , 5 , 6]
+T = [0 , 0],[0 , 0],[0 , 0]
+for i in range (len(A)):
+    for j in range (len (A[0])):
+        T[j][i] = A [i][j]
+for i in T:
+    print(i)
