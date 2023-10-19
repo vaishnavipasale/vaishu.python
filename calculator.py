f3e22904f0a326b673d7418cc3a9dcbbecc2504f
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-19 17:03:18 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+Create a simple calculator that takes user input for two numbers and an operation (+, -, *, /)
+num = int (input("Enter a first number"))
+num1 = int(input("Enter a second number"))
+print("press 1 for addition")
+print("press 2 for addition")
+print("press 3 for addition")
+print("press 4 for addition")
+choice = int(input("Enter a choice"))
+if choice ==1:
+    print(num+num1)
+elif choice ==2:
+    print(num-num1)
+elif choice ==3:
+    print(num*num1)
+elif choice ==4:
+    print(num/num1)
+else:
+    print("invalid number")
