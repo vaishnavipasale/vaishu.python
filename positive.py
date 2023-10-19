ab2e379129a3baeb94dd90e1bc835df393108315
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-19 16:43:52 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,9 @@
+Write a program to check if a number is positive, negative, or zero.
+num1 = int(input("Enter a first number"))
+
+if num1>0:
+    print(f"{num1} is positive")
+elif num1==0:
+    print(f"{num1} is zero")
+else:
+    print(f"{num2} is negative")
