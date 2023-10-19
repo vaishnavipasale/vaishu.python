3a6b271a0cc93183edbe88989a626be0c550f95a
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-19 16:39:15 +0530

    Create areareactangle.py

---
+++ areareactangle.py
@@ -0,0 +1,7 @@
+Calculate the area of a rectangle given its length and width.?
+length = int(input("Enter a length"))
+breadth= int(input("Enter a breadth"))
+print(f"length is {length}")
+print(f"breadth is {breadth}")
+area = length * breadth
+print(f"area is {area}")
