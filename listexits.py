c31b0e84f6b4a5adb7c99a05f08ed0a89139e8ca
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-19 16:58:10 +0530

    Create listexits.py

---
+++ listexits.py
@@ -0,0 +1,7 @@
+Check if an element exists in a list.
+list1 = [100, 20, 330 , 4 , 45 , 0]
+i = int(input("enter a i"))
+if i in list1:
+    print(f"{i}  is exits in list")
+else:
+    print(f"{i} is not exits in list")
