662b57cbfb01c603f064874521d5d81b12c7369e
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-19 16:35:35 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,11 @@
+Write a program to swap two variables?
+num1= int(input("Enter a first number"))
+num2 = int(input("Enter a second number"))
+print(f"number before swapping {num1}")
+print(f"numberbefore swapping {num2}")
+
+num3 = num1
+num2 = num3
+num2 = num1
+print(f"number after swapping {num1}")
+print(f"number after swapping {num2}")
