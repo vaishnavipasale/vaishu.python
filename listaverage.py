da01a749a0f4425ab685095e9175195e7fb9e8ec
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-19 16:56:34 +0530

    Create listaverage.py

---
+++ listaverage.py
@@ -0,0 +1,5 @@
+Create a list of numbers and find the sum and average?
+list1 = [10, 20, 33]
+print(f"sum of list is :" ,sum(list1))
+average = sum(list1) / len(list1)
+print(f"average of list is : {average}")
