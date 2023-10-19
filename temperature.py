1efa906a9be8db9e2aac54e4dbb3638caa8e1757
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-19 16:37:28 +0530

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,4 @@
+Convert the temperature from Celsius to Fahrenheit.
+temperature = float(input("enter a temperture in fahreheit"))
+celsius = (temperature - 32)* 5 / 9
+print("Temperture in celsius :" , celsius)
