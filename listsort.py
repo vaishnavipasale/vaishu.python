b517bb7d8552f05f538379c1167b94e6e0aaeba7
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-21 19:51:30 +0530

    Create listsort.py

---
+++ listsort.py
@@ -0,0 +1,15 @@
+.Merge two sorted lists into one sorted list.
+
+list1 = [1, 5, 6, 9, 11]
+list2 = [3, 4, 7, 8, 10]
+ 
+
+print (f"The original list 1 is : {list1}")
+print (f"The original list 2 is : {list2}")
+ 
+
+res = sorted(list1 + list2)
+ 
+
+print (f"The combined sorted list is : {res}")
+
