35aff3e5b404586549ff6262193b1c2d417f2bf7
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-22 14:23:21 +0530

    Create averagedict.py

---
+++ averagedict.py
@@ -0,0 +1,4 @@
+Calculate the average of values in a dictionary
+student = {'Jack': 85,'Jill': 78, 'Steve': 92, 'Anna': 87, 'Leo': 95,}
+average = sum(student.values()) / len(student)
+print(f"The average grade is: {average}")
