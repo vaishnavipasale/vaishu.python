8cadf559effdba8dc91dbd47a0a6f8bde7e9e78a
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-22 19:50:14 +0530

    Create existsdict.py

---
+++ existsdict.py
@@ -0,0 +1,8 @@
+.Check if a given key exists in a dictionary
+dict = {1: "swara" , 2: "sakshi", 3: "sneha", 4:"vaishnavi"}
+dict1 = "harsh"
+if dict1 in dict.keys():
+    print("keys exits")
+else:
+          print("keys not exits")
+
