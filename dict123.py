ebeec48aad04b0483ff898fefcdc78c463b07ff5
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-22 20:25:45 +0530

    Create dict123.py

---
+++ dict123.py
@@ -0,0 +1,5 @@
+Merge two dictionaries while summing values for common keys
+dict1 = {1: "swara", 2:"sakshi" , 3: "vaishnavi"}
+dict2= {4:"harsh", 6:"avir", 7:"jay"}
+dict3 = {**dict1, **dict2}
+print(dict3)
