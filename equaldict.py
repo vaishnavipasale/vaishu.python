86215eea05258cde1f2c2f8fde8e1906669c20a6
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-22 16:02:28 +0530

    Create equaldict.py

---
+++ equaldict.py
@@ -0,0 +1,7 @@
+Check if two dictionaries have the same keys and values.
+dict1 = {1: "swara" , 2: "sakshi" , 3: "sneha" , 4:"vaishnavi"}
+dict2 = {1: "swara" , 2: "sakshi" , 3: "sneha" , 4:"vaishnavi"}
+if dict1 == dict2:
+    print("Two dictionary are eaqual")
+else:
+    print("not equal")
