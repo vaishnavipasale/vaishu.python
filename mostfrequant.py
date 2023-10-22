8ba1c4ae17374bed2ed03efedfff0ff9efaddd83
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-22 16:10:49 +0530

    Create mostfrequant.py

---
+++ mostfrequant.py
@@ -0,0 +1,15 @@
+Find the mode (most frequent element) in a list.
+def most_frequent(List):
+    counter = 0
+    num = List[0]
+     
+    for i in List:
+        curr_frequency = List.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+ 
+    return num
+ 
+List = [2, 1, 2, 2, 1, 3]
+print(most_frequent(List))
