b99760da1efffb2fe512c4f151f5181b9d02db60
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-22 19:00:53 +0530

    Create listsequal.py

---
+++ listsequal.py
@@ -0,0 +1,8 @@
+.Check if two lists are equal (have the same elements in the same order).
+
+list1 = [10, 10, 11, 12, 12, 13, 14, 16, 15, 16, 12]
+list2 = [16, 12, 13, 14, 15, 16, 10, 11, 12, 10, 12]
+if list1 == list2:
+    print('Lists are equal i.e. contain similar elements with same frequency')
+else:
+    print('Lists are not equal')
