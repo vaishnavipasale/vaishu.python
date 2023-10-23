f4efbfa61261d24877a61d1ab83302e69e09ddbc
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-23 19:39:17 +0530

    Update square.py

--- square.py
+++ square.py
@@ -1,1 +1,7 @@
 Print a pattern of numbers in a square.
+row = int(input("Enter number of rows: "))
+print("Square pattern is: ")
+for i in range(1,row+1):
+    for j in range(1,row+1):
+        print("*", end="")
+    print()
