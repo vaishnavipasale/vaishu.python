cc1cbf2467035c1212f3fdfeef2779183033e5e6
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-23 20:11:56 +0530

    Update matrixdeter.py

--- matrixdeter.py
+++ matrixdeter.py
@@ -1,1 +1,9 @@
+Find the determinant of a square matrix
+import numpy as np 
+n_array = np.array([[50, 29], [30, 44]]) 
+print("Numpy Matrix is:") 
+print(n_array) 
+det = np.linalg.det(n_array) 
+print("\nDeterminant of given 2X2 matrix:") 
+print(int(det))
 
