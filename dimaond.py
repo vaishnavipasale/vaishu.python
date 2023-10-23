c1bc8b6751d8b72680a99c0e5e8e8275ddb047a8
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-23 19:36:40 +0530

    Update dimaond.py

--- dimaond.py
+++ dimaond.py
@@ -1,1 +1,15 @@
 Print a pattern of stars in a diamond shape.
+num_rows = 5
+for i in range(1, num_rows + 1):
+    for j in range(num_rows - i):
+        print(" ", end="")
+      for j in range(2 * i - 1):
+        print("*", end="")
+print()
+for i in range(num_rows - 1, 0, -1):
+   for j in range(num_rows - i):
+        print(" ", end="")
+     for j in range(2 * i - 1):
+        print("*", end="")
+print()
+
