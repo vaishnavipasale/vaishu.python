b3594e866fe10b8d14ee06856ccc9f929d681d96
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-23 18:31:34 +0530

    Update dictmerage.py

--- dictmerage.py
+++ dictmerage.py
@@ -1,1 +1,11 @@
+Merge two dictionaries while summing values for common keys.
+dict1 = {'a': 12, 'for': 25, 'c': 9}
+dict2 = {"as":12, "while":25 , "abc":9}
+for key in dict2:
+    if key in dict1:
+        dict2[key] = dict2[key] + dict1[key]
+    else:
+        pass
+         
+print(dict2)
 
