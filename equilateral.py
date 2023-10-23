53fce71c373140f40bdf63b8d9dc1a16fb97edcd
vaishnavipasale <147927413+vaishnavipasale@users.noreply.github.com>
2023-10-23 19:30:47 +0530

    Update equilateral.py

--- equilateral.py
+++ equilateral.py
@@ -1,1 +1,7 @@
 Print a pattern of numbers in an equilateral triangle
+n=20
+for i in range(1, 11):
+    print(' '*n, end='') # repet space for n times
+    print('* '*(i)) # repeat stars for i times
+    n-=1
+
